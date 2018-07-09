e52c2626c7d9016725909ff629c79e2b06d66e30
gui2524 <garaujo@stone.com.br>
2018-07-08 23:07:16 -0300

    1 Corrigindo identacao

--- img_compare.py
+++ img_compare.py
@@ -15,28 +15,28 @@ global_imageCounter = 0
 """-------------FUNCTIONS------------"""
 
 def takePhoto(imagePath, cameraId):
-	comando = "fswebcam -d /dev/video" + str(cameraId) + " " + imagePath
-	system(comando)
+    comando = "fswebcam -d /dev/video" + str(cameraId) + " " + imagePath
+    system(comando)
 
 def getImagePath(imageId):
-	return "img-" + str(imageId) + ".jpeg"
+    return "img-" + str(imageId) + ".jpeg"
 
 def getImageHist(imagePath):
-	image = cv2.imread(imagePath)
-	hist = cv2.calcHist(image, [0], None, [8],[0, 256])
-	hist = cv2.normalize(hist).flatten()
-	print(hist)
-	return hist
+    image = cv2.imread(imagePath)
+    hist = cv2.calcHist(image, [0], None, [8],[0, 256])
+    hist = cv2.normalize(hist).flatten()
+    print(hist)
+    return hist
 
 def compareImages(firstImagePath, secondImagePath):
-	firstHist  = getImageHist(firstImagePath)
-	secondHist = getImageHist(secondImagePath)
-	diff = cv2.compareHist(firstHist, secondHist, method=cv2.CV_COMP_CHISQR)
-	print(diff)
-	if diff >= HISTOGRAM_DIFF_THRESHOLD:
-		return True
-	else:
-		return False
+    firstHist  = getImageHist(firstImagePath)
+    secondHist = getImageHist(secondImagePath)
+    diff = cv2.compareHist(firstHist, secondHist, method=cv2.CV_COMP_CHISQR)
+    print(diff)
+    if diff >= HISTOGRAM_DIFF_THRESHOLD:
+        return True
+    else:
+        return False
 
 
 
--- notification.py
+++ notification.py
@@ -12,56 +12,55 @@ global_nextUpdateId = 0
 """-------------FUNCTIONS------------"""
 
 def sendMessage(message):
-	address = global_baseAddress + "/sendMessage"
-	data = {"chat_id": global_chatId, "text": message}
-	response = post(address, data=data)
-	print(response)
+    address = global_baseAddress + "/sendMessage"
+    data = {"chat_id": global_chatId, "text": message}
+    response = post(address, data=data)
+    print(response)
 
 def sendPhoto(imagePath):
-	address = global_baseAddress + "/sendPhoto"
-	data = {"chat_id": global_chatId}
-	photoFile = {"photo": open("foto.jpeg", "rb")}
-	response = post(address, data=data, files=photoFile)
-	print(response)
+    address = global_baseAddress + "/sendPhoto"
+    data = {"chat_id": global_chatId}
+    photoFile = {"photo": open("foto.jpeg", "rb")}
+    response = post(address, data=data, files=photoFile)
+    print(response)
 
 def getUpdates():
-	address = global_baseAddress + "/getUpdates"
-	data = {"offset": global_nextUpdateId}
-	response = get(address, json=data)
-	print(response)
+    address = global_baseAddress + "/getUpdates"
+    data = {"offset": global_nextUpdateId}
+    response = get(address, json=data)
+    print(response)
 	
-	if(response == None):
-		return None
+    if(response == None):
+        return None
 	
     responseDict = response.json()
 
-
     for result in responseDict["result"]:
         message = result["message"]
-		if "text" in message:
-			text = message["text"]
-		elif "voice" in message:
-			fileId = message["voice"]["file_id"]
-		elif "photo" in message:
-			photo = message["photo"][-1]
-			photoId = photo["file_id"]
+        if "text" in message:
+            text = message["text"]
+        elif "voice" in message:
+            fileId = message["voice"]["file_id"]
+        elif "photo" in message:
+            photo = message["photo"][-1]
+            photoId = photo["file_id"]
 			
-	global_nextUpdateId = int(result["update_id"]) + 1
+    global_nextUpdateId = int(result["update_id"]) + 1
 	
 def getFile(fileId, fileName):
-	address = global_baseAddress + "/getFile"
-	data = {"file_id": fileId}
-	response = get(endereco, json=data)
-	print(response)
-	
-	if(response == null):
-		return null
-		
-	responseDict = response.json()
-	filePath = responseDict["result"]["file_path"]
+    address = global_baseAddress + "/getFile"
+    data = {"file_id": fileId}
+    response = get(endereco, json=data)
+    print(response)
 
-	fileAddress = "https://api.telegram.org/file/bot" + global_key + "/" + filePath
-	urlretrieve(fileAddress, fileName)
+    if(response == null):
+    return null
+
+    responseDict = response.json()
+    filePath = responseDict["result"]["file_path"]
+
+    fileAddress = "https://api.telegram.org/file/bot" + global_key + "/" + filePath
+    urlretrieve(fileAddress, fileName)
 	
 	
 """-------------TEST------------"""
