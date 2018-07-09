461d51af8ac6cbc5c985634b058aff45a6d891f1
gui2524 <garaujo@stone.com.br>
2018-07-08 22:37:19 -0300

    2 Alterando identacao e corrigindo

--- log.py
+++ log.py
@@ -8,21 +8,21 @@ TIME_FORMAT = "%H:%M:%S"
 """-------------FUNCTIONS------------"""
 
 def formatText(text):
-	time = datetime.now()
-	timeFormatted = time.strftime(TIME_FORMAT)
-	textFormatted = timeFormatted + " ---> " + text + "\n"
-	return textFormatted
-	
+    time = datetime.now()
+    timeFormatted = time.strftime(TIME_FORMAT)
+    textFormatted = timeFormatted + " ---> " + text + "\n"
+    return textFormatted
+
 
 def log(text):
-	logText = formatText(text)
-	print(logText)
-	
+    logText = formatText(text)
+    print(logText)
+
 def eventLog(text):
-	logText = formatText(text)
-	log(text)
-	with open(LOG_FILE, "w") as textFile:
-		textFile.write(logText)
-		
-		
+    logText = formatText(text)
+    log(text)
+    with open(LOG_FILE, "a") as textFile:
+		    textFile.write(logText)
+
+
 """-------------TEST------------"""
